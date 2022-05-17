c9d448dd8d1675131f2b2c78349cea81e1fd40a5
yaman <rakolee1999sawan@mail.com>
2022-05-17 23:15:07 +0300

    problem solving js

---
+++ _js_part/class-3/p1.js
@@ -0,0 +1,78 @@
+/*********************************************** */
+
+// 1- declare two varibales from type string
+// 2- add custom values
+// 3- make compare between them if they equal each others if yes preint "equal" if no print "not equal"
+
+// let text = 'fawaz';
+// let text2 = 'mohammad';
+
+// if (text == text2) {
+//   console.log('equal');
+// } else if (text === text2) {
+//   console.log('equal type');
+// } else {
+//   console.log('not equal');
+// }
+
+/*********************************************** */
+
+// print numbers from 1 to 10
+
+// for (let i = 0; i <= 10; i += 2) {
+//   if (i % 2 == 0) for{
+//     console.log(i);
+//   }
+// }
+
+/********************************************* */
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+/********************************************** */
+// let numbers = [5, 6, 13, 9 ];
+// let result = 0 ;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     result += numbers[i];
+// }
+// console.log(result);
+
+/***************************************************** */
+
+// create a function that takes two numbers and return the sum of them
+
+// function calculate(number1, number2 , operation) {
+//   let result = 0;
+//   if (operation == '*') {
+//     result = number1 * number2;
+//   } else if (operation == '+') {
+//     result = number1 + number2;
+//   }
+//   return result;
+// }
+
+// let value = calculate(5, 10 , "+");
+// console.log(value);
+
+
+/************************************************************* */
+// create function take 1 parametr as string and this parametr it will be a text
+// you should take the text and split each char and add it to array
+// fawaz => ["f" , "a" , "w" , "a" , "z"]
+
+// function splitChars(text) {
+//   let result = [];
+//   for (let i = 0; i < text.length; i++) {
+//     // result[i] = text[i];
+
+//     // result.push(text[i]);
+
+//     // result = text.split("");
+//   }
+//   return result;
+// }
+
+// let result = splitChars('mohammed');
+// console.log(result);
